50afe85c6a78ba9f8224a94e385521c256a33957
Jesse Stone <jstone@psi.edu>
2024-08-19 13:24:21 -0700

    added a method that should validate modification_histories

--- validator.py
+++ validator.py
@@ -1,13 +1,15 @@
 import pds4
+import label
+import pds4types
 from typing import Dict
 
 
 def check_collection_increment(previous: pds4.CollectionInventory, next: pds4.CollectionInventory):
-    check_dict_increment(previous.primary, next.primary)
-    check_dict_increment(previous.secondary, next.secondary)
+    _check_dict_increment(previous.primary, next.primary)
+    _check_dict_increment(previous.secondary, next.secondary)
 
 
-def check_dict_increment(previous: Dict[pds4.Lid, pds4.LidVid], next: Dict[pds4.Lid, pds4.LidVid]):
+def _check_dict_increment(previous: Dict[pds4.Lid, pds4.LidVid], next: Dict[pds4.Lid, pds4.LidVid]):
     for lid in next.keys():
         if lid in previous.keys():
             lidvid: pds4.LidVid = next[lid]
@@ -17,3 +19,32 @@ def check_dict_increment(previous: Dict[pds4.Lid, pds4.LidVid], next: Dict[pds4.
                 raise Exception(f"Invalid lidvid: {lidvid}. Must be one of {allowed}")
 
 
+def check_for_modification_history(lbl: label.ProductLabel):
+    lidvid = pds4.LidVid.parse(lbl.identification_area.lidvid)
+    vid = lidvid.vid.__str__()
+    if lbl.identification_area.modification_history is None:
+        raise Exception(f"{lidvid} does not have a modification history")
+
+    versions = [detail.version_id for detail in lbl.identification_area.modification_history.modification_details]
+    if vid not in versions:
+        raise Exception(f'{lidvid} does not have a current modification history. Versions seen were: {versions}')
+
+
+def check_for_modification_history(previous: label.ProductLabel, next: label.ProductLabel):
+    previous_details = previous.identification_area.modification_history.modification_details
+    next_details = next.identification_area.modification_history.modification_details
+
+    next_lidvid = next.identification_area.lidvid
+    prev_lidvid = previous.identification_area.lidvid
+
+    if len(next_details) == len(previous_details) + 1:
+        pairs = zip(previous_details, next_details[:len(previous_details)])
+        for pair in pairs:
+            previous_detail: pds4types.ModificationDetail
+            next_detail: pds4types.ModificationDetail
+            previous_detail, next_detail = pair
+
+            if not previous_detail == next_detail:
+                raise Exception(f'{next_lidvid} has a mismatched modification detail from {prev_lidvid}. The old modification detail was {previous_detail}, and the new one was {next_detail}')
+    else:
+        raise Exception(f"{next_lidvid} must contain one more modification detail than {prev_lidvid}")
