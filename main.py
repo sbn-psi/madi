beb40770c46d46867c53cea599ca5435553fa544
Jesse Stone <jstone@psi.edu>
2024-08-22 12:18:55 -0700

    extracted load_remote_bundle

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ import webclient
 
 def main():
     url = "http://localhost:8000/orex.tagcams_v1.0/"
+    bundles, collections, products = load_remote_bundle(url)
+
+    for b in bundles:
+        print(webclient.remote_checksum(b.url))
+        print(b.label.checksum)
+
+
+def load_remote_bundle(url):
     root = webclient.fetchdir(url)
     label_urls = [x for x in root.flat_files() if x.endswith(".xml")]
     product_urls = [x for x in label_urls if is_basic(x)]
@@ -15,9 +23,7 @@ def main():
     bundles = [webclient.fetchbundle(url) for url in bundle_urls]
     products = [webclient.fetchproduct(url) for url in product_urls]
 
-    for b in bundles:
-        print(webclient.remote_checksum(b.url))
-        print(b.label.checksum)
+    return bundles, collections, products
 
 def is_basic(x):
     return not (is_collection(x) or is_bundle(x))
