2420f4d5d4e995edd14b0e90174afbe978c878d5
Jesse Stone <jstone@psi.edu>
2024-08-23 13:48:10 -0700

    updated make_absolute to check for already-absolute paths

--- webclient.py
+++ webclient.py
@@ -125,9 +125,16 @@ def rebase_filenames(basepath, filenames):
 
 
 def make_absolute(base: str, candidate: str) -> str:
-    if candidate.startswith(base):
-        return candidate
+    if is_absolute(candidate):
+        if candidate.startswith(base):
+            return candidate
+        else:
+            raise Exception(f"{candidate} cannot be made absolute. It is already absolute, and not below {base}")
     return base + candidate
+
+
+def is_absolute(candidate: str):
+    return "://" in candidate
 
 
 def is_below(base: str, candidate: str) -> bool:
