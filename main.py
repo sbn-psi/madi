88a6e6382ac17810691c97a3640427857907baf8
Jesse Stone <jstone@psi.edu>
2024-09-11 15:41:31 -0700

    extracted bundle and collection comparison methods

--- main.py
+++ main.py
@@ -19,21 +19,32 @@ def main():
         print(webclient.remote_checksum(bundle.url))
         print(bundle.label.checksum)
 
-    validator.check_bundle_increment(previous_bundles[0].label, new_bundles[0].label)
-
-    new_collection_lidvids = [LidVid.parse(x.label.identification_area.lidvid) for x in new_collections]
-    validator.check_bundle_for_latest_collections(new_bundles[0].label, set(new_collection_lidvids))
+    check_bundle_against_previous(previous_bundles[0], new_bundles[0])
+    check_bundle_against_collections(new_bundles[0], new_collections)
 
     for new_collection in new_collections:
         new_collection_lid = LidVid.parse(new_collection.label.identification_area.lidvid).lid
         previous_collections = [x for x in previous_collections if LidVid.parse(x.label.identification_area.lidvid).lid == new_collection_lid]
         if previous_collections:
             previous_collection = previous_collections[0]
-            validator.check_for_modification_history(previous_collection.label)
-            validator.check_for_modification_history(new_collection.label)
-            validator.check_for_preserved_modification_history(previous_collection.label, new_collection.label)
-            validator.check_collection_increment(previous_collection.inventory, new_collection.inventory)
-            validator.check_collection_duplicates(previous_collection.inventory, new_collection.inventory)
+            check_collection_against_previous(previous_collection, new_collection)
+
+
+def check_bundle_against_previous(previous_bundle, new_bundle):
+    validator.check_bundle_increment(previous_bundle.label, new_bundle.label)
+
+
+def check_bundle_against_collections(bundle, collections):
+    collection_lidvids = [LidVid.parse(x.label.identification_area.lidvid) for x in collections]
+    validator.check_bundle_for_latest_collections(bundle.label, set(collection_lidvids))
+
+
+def check_collection_against_previous(previous_collection, new_collection):
+    validator.check_for_modification_history(previous_collection.label)
+    validator.check_for_modification_history(new_collection.label)
+    validator.check_for_preserved_modification_history(previous_collection.label, new_collection.label)
+    validator.check_collection_increment(previous_collection.inventory, new_collection.inventory)
+    validator.check_collection_duplicates(previous_collection.inventory, new_collection.inventory)
 
 
 def load_remote_bundle(url):
