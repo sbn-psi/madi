61baec72920476ad90898e482759c4d3ad0c0d35
Jesse Stone <jstone@psi.edu>
2024-10-08 11:33:47 -0700

    improved type hints

--- localclient.py
+++ localclient.py
@@ -1,36 +1,39 @@
 import hashlib
 import itertools
 import os
+from typing import Iterable
 
 import bs4
 
 import paths
 import product
-import pds4
+from labeltypes import ProductLabel
+
+from pds4 import CollectionProduct, BundleProduct, BasicProduct, CollectionInventory
 
 
-def fetchcollection(path):
+def fetchcollection(path: str) -> CollectionProduct:
     collection_label = fetchlabel(path)
     inventory_path = os.path.join(os.path.dirname(path), collection_label.file_area.file_name)
     with open(inventory_path, newline="") as f:
-        inventory = pds4.CollectionInventory.from_csv(f.read())
-    return pds4.CollectionProduct(collection_label, inventory, label_path=path, inventory_path=inventory_path)
+        inventory = CollectionInventory.from_csv(f.read())
+    return CollectionProduct(collection_label, inventory, label_path=path, inventory_path=inventory_path)
 
 
-def fetchbundle(path):
+def fetchbundle(path: str) -> BundleProduct:
     bundle_label = fetchlabel(path)
-    return pds4.BundleProduct(bundle_label, path=path)
+    return BundleProduct(bundle_label, path=path)
 
 
-def fetchproduct(path):
+def fetchproduct(path: str) -> BasicProduct:
     product_label = fetchlabel(path)
     data_paths = paths.rebase_filenames(path, [product_label.file_area.file_name]) if product_label.file_area else []
     document_paths = paths.rebase_filenames(path, product_label.document.filenames()) if product_label.document else []
 
-    return pds4.BasicProduct(product_label, label_path=path, data_paths=data_paths + document_paths)
+    return BasicProduct(product_label, label_path=path, data_paths=data_paths + document_paths)
 
 
-def fetchlabel(path):
+def fetchlabel(path: str) -> ProductLabel:
     with open(path) as f:
         text = f.read()
         checksum = hashlib.md5(text.encode('utf-8')).hexdigest()
@@ -38,7 +41,7 @@ def fetchlabel(path):
         return product.extract_label(soup, checksum, path)
 
 
-def get_file_paths(path):
+def get_file_paths(path: str) -> Iterable[str]:
     return itertools.chain.from_iterable(
         (os.path.join(dirpath, filename) for filename in filenames)
         for dirpath, _, filenames in os.walk(path))
