d0181d5d026e5a94b32c47ca97df61ab842a0686
Jesse Stone <jstone@psi.edu>
2024-10-25 09:39:45 -0700

    extracted ready.py

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ from dataclasses import dataclass
 
 import paths
 import pds4
-import validator
-import localclient
 
 from typing import Iterable, List, Tuple
 from pds4 import BundleProduct, CollectionProduct, BasicProduct
 import argparse
-import logging
 from bundleloader import load_local_bundle
 
+
+from ready import check_ready
+
+import logging
 logger = logging.getLogger(__name__)
 
 
@@ -222,36 +223,7 @@ def find_superseded(previous_products: List[pds4.Pds4Product],
     return previous_products_to_keep, previous_products_to_supersede
 
 
-def check_ready(previous_bundle_directory, new_bundle_directory) -> None:
-    logger.info(f"Checking readiness of new bundle {new_bundle_directory} against {previous_bundle_directory}")
 
-    previous_fullbundle = load_local_bundle(previous_bundle_directory)
-    for bundle in previous_fullbundle.bundles:
-        logger.info(f'Previous bundle checksum: {bundle.label.checksum}')
-
-    new_fullbundle = load_local_bundle(new_bundle_directory)
-    for bundle in new_fullbundle.bundles:
-        logger.info(f'New bundle checksum: {bundle.label.checksum}')
-
-    errors = do_checkready(new_fullbundle, previous_fullbundle)
-    if len(errors) > 0:
-        for e in errors:
-            logger.error(e.message)
-        raise Exception("Validation errors encountered")
-
-
-def do_checkready(new_fullbundle: pds4.FullBundle, previous_fullbundle: pds4.FullBundle) -> List[validator.ValidationError]:
-    errors = []
-    errors.extend(validator.check_bundle_against_previous(previous_fullbundle.bundles[0], new_fullbundle.bundles[0]))
-    errors.extend(validator.check_bundle_against_collections(new_fullbundle.bundles[0], new_fullbundle.collections))
-    for new_collection in new_fullbundle.collections:
-        new_collection_lid = new_collection.label.identification_area.lidvid.lid
-        previous_collections = [x for x in previous_fullbundle.collections if
-                                x.label.identification_area.lidvid.lid == new_collection_lid]
-        if previous_collections:
-            previous_collection = previous_collections[0]
-            errors.extend(validator.check_collection_against_previous(previous_collection, new_collection))
-    return errors
 
 
 
