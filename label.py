1779086beca2fd76c1416360c386746f2f238d55
Jesse Stone <jstone@psi.edu>
2024-10-31 09:00:51 -0700

    added awareness of Product_Bundle/File_Area_Text

--- label.py
+++ label.py
@@ -33,7 +33,8 @@ def extract_bundle(bundle: bs4.Tag, checksum: str) -> ProductLabel:
         checksum=checksum,
         identification_area=_extract_identification_area(bundle.Identification_Area),
         context_area=_extract_context_area(bundle.Context_Area),
-        bundle_member_entries=[_extract_bundle_member_entry(x) for x in bundle.find_all("Bundle_Member_Entry")]
+        bundle_member_entries=[_extract_bundle_member_entry(x) for x in bundle.find_all("Bundle_Member_Entry")],
+        file_area=_extract_file_area(bundle.File_Area_Text)
     )
 
 
