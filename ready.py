149f994723cadfa462739695b0a52aaca33cb511
Jesse Stone <jstone@psi.edu>
2024-11-06 08:45:28 -0700

    use "delta" instead of "new" where applicable

--- ready.py
+++ ready.py
@@ -8,21 +8,21 @@ logger = logging.getLogger(__name__)
 
 
 
-def check_ready(previous_fullbundle, new_fullbundle) -> None:
+def check_ready(previous_fullbundle, delta_fullbundle) -> None:
     previous_bundle_directory = previous_fullbundle.path
-    new_bundle_directory = new_fullbundle.path
+    delta_bundle_directory = delta_fullbundle.path
 
-    logger.info(f"Checking readiness of new bundle {new_bundle_directory} against {previous_bundle_directory}")
+    logger.info(f"Checking readiness of delta bundle {delta_bundle_directory} against {previous_bundle_directory}")
 
     for bundle in previous_fullbundle.bundles:
         logger.info(f'Previous bundle checksum: {bundle.label.checksum}')
 
-    for bundle in new_fullbundle.bundles:
-        logger.info(f'New bundle checksum: {bundle.label.checksum}')
+    for bundle in delta_fullbundle.bundles:
+        logger.info(f'Delta bundle checksum: {bundle.label.checksum}')
 
-    errors = do_checkready(new_fullbundle, previous_fullbundle)
+    errors = do_checkready(delta_fullbundle, previous_fullbundle)
 
-    logger.info(f"Checking readiness of new bundle {new_bundle_directory} against {previous_bundle_directory} - Complete")
+    logger.info(f"Checking readiness of delta bundle {delta_bundle_directory} against {previous_bundle_directory} - Complete")
 
     if len(errors) > 0:
         for e in errors:
@@ -30,15 +30,15 @@ def check_ready(previous_fullbundle, new_fullbundle) -> None:
         raise Exception("Validation errors encountered")
 
 
-def do_checkready(new_fullbundle: pds4.FullBundle, previous_fullbundle: pds4.FullBundle) -> List[validator.ValidationError]:
+def do_checkready(delta_fullbundle: pds4.FullBundle, previous_fullbundle: pds4.FullBundle) -> List[validator.ValidationError]:
     errors = []
-    errors.extend(validator.check_bundle_against_previous(previous_fullbundle.bundles[0], new_fullbundle.bundles[0]))
-    errors.extend(validator.check_bundle_against_collections(new_fullbundle.bundles[0], new_fullbundle.collections))
-    for new_collection in new_fullbundle.collections:
-        new_collection_lid = new_collection.label.identification_area.lidvid.lid
+    errors.extend(validator.check_bundle_against_previous(previous_fullbundle.bundles[0], delta_fullbundle.bundles[0]))
+    errors.extend(validator.check_bundle_against_collections(delta_fullbundle.bundles[0], delta_fullbundle.collections))
+    for delta_collection in delta_fullbundle.collections:
+        new_collection_lid = delta_collection.label.identification_area.lidvid.lid
         previous_collections = [x for x in previous_fullbundle.collections if
                                 x.label.identification_area.lidvid.lid == new_collection_lid]
         if previous_collections:
             previous_collection = previous_collections[0]
-            errors.extend(validator.check_collection_against_previous(previous_collection, new_collection))
+            errors.extend(validator.check_collection_against_previous(previous_collection, delta_collection))
     return errors
