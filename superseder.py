4fbab778cecb3287ed909befedb1048b71d084f5
Jesse Stone <jstone@psi.edu>
2024-11-07 14:19:01 -0700

    Added a dry option that will avoid writing to the filesystem

--- superseder.py
+++ superseder.py
@@ -12,7 +12,7 @@ import re
 logger = logging.getLogger(__name__)
 
 
-def supersede(previous_fullbundle: pds4.FullBundle, delta_fullbundle: pds4.FullBundle, merged_bundle_directory) -> None:
+def supersede(previous_fullbundle: pds4.FullBundle, delta_fullbundle: pds4.FullBundle, merged_bundle_directory, dry: bool) -> None:
     """
     Merges the bundles together and supersedes any products that have a newer version.
     """
@@ -66,9 +66,9 @@ def supersede(previous_fullbundle: pds4.FullBundle, delta_fullbundle: pds4.FullB
                                   delta_fullbundle.bundles,
                                   delta_fullbundle.products), delta_bundle_directory, merged_bundle_directory)
 
-    do_copy_data(previous_products_to_keep, previous_bundle_directory, merged_bundle_directory)
-    do_copy_data(previous_products_to_supersede, previous_bundle_directory, merged_bundle_directory, superseded=True)
-    do_copy_data(delta_fullbundle.products, delta_bundle_directory, merged_bundle_directory)
+    do_copy_data(previous_products_to_keep, previous_bundle_directory, merged_bundle_directory, dry)
+    do_copy_data(previous_products_to_supersede, previous_bundle_directory, merged_bundle_directory, dry, superseded=True)
+    do_copy_data(delta_fullbundle.products, delta_bundle_directory, merged_bundle_directory, dry)
 
     do_copy_readme(previous_fullbundle.superseded_bundles, previous_bundle_directory, merged_bundle_directory, superseded=True)
     do_copy_readme(previous_fullbundle.bundles, previous_bundle_directory, merged_bundle_directory, superseded=True)
@@ -76,26 +76,29 @@ def supersede(previous_fullbundle: pds4.FullBundle, delta_fullbundle: pds4.FullB
 
     do_copy_inventory(previous_collections_to_supersede, previous_bundle_directory, merged_bundle_directory, superseded=True)
 
-    copy_unmodified_collections(previous_collections_to_keep, previous_bundle_directory, delta_bundle_directory)
+    copy_unmodified_collections(previous_collections_to_keep, previous_bundle_directory, delta_bundle_directory, dry)
     generate_collections(previous_collections_to_supersede,
                          delta_fullbundle.collections,
                          previous_bundle_directory,
                          delta_bundle_directory,
-                         merged_bundle_directory)
+                         merged_bundle_directory,
+                         dry)
 
     copy_previously_superseded_products(
         previous_fullbundle.superseded_products,
         previous_fullbundle.superseded_collections,
         previous_fullbundle.superseded_bundles,
         previous_bundle_directory,
-        merged_bundle_directory)
+        merged_bundle_directory,
+        dry)
 
 
 def generate_collections(previous_collections_to_supersede: List[pds4.Pds4Product],
                          delta_collections: List[pds4.CollectionProduct],
                          previous_bundle_directory: str,
                          delta_bundle_directory: str,
-                         merged_bundle_directory: str) -> None:
+                         merged_bundle_directory: str,
+                         dry: bool) -> None:
     """
     Matches up previous and delta collections and merges their inventories.
     """
@@ -107,14 +110,15 @@ def generate_collections(previous_collections_to_supersede: List[pds4.Pds4Produc
             delta_collection = [x for x in delta_collections
                                 if x.label.identification_area.lidvid.lid == previous_collection_lid][0]
             generate_collection(previous_collection, delta_collection, previous_bundle_directory, delta_bundle_directory,
-                                merged_bundle_directory)
+                                merged_bundle_directory, dry)
 
 
 def generate_collection(previous_collection: pds4.CollectionProduct,
                         delta_collection: pds4.CollectionProduct,
                         previous_bundle_directory: str,
                         delta_bundle_directory: str,
-                        merged_bundle_directory: str) -> None:
+                        merged_bundle_directory: str,
+                        dry: bool) -> None:
     """
     Merges the inventories from the previous and delta collection and updates the label file with the new
     record count.
@@ -129,15 +133,24 @@ def generate_collection(previous_collection: pds4.CollectionProduct,
     inventory_path = paths.relocate_path(previous_collection.inventory_path,
                                          previous_bundle_directory,
                                          merged_bundle_directory)
-    logger.info(f"Writing merged inventory to {inventory_path}")
-    with open(inventory_path, 'w') as f:
-        f.write(inventory.to_csv())
+
+    if not dry:
+        logger.info(f"Writing merged inventory to {inventory_path}")
+        with open(inventory_path, 'w') as f:
+            f.write(inventory.to_csv())
+    else:
+        logger.info(f"Skipped: Writing merged inventory to {inventory_path}")
+
     inventory_label_contents = open(delta_collection.label_path).read()
     new_inventory_label_contents = re.sub(r"<records>\d*</records>", f"<records>{product_count}</records>",
                                           inventory_label_contents)
     new_path = paths.relocate_path(delta_collection.label_path, delta_bundle_directory, merged_bundle_directory)
-    logger.info(f"Writing new inventory label to f{new_path}")
-    open(new_path, "w").write(new_inventory_label_contents)
+
+    if not dry:
+        logger.info(f"Writing new inventory label to f{new_path}")
+        open(new_path, "w").write(new_inventory_label_contents)
+    else:
+        logger.info(f"Skipped: Writing new inventory label to f{new_path}")
 
 
 def report_superseded(products_to_keep: List[pds4.Pds4Product],
@@ -187,36 +200,37 @@ def copy_previously_superseded_products(
         collections: Iterable[pds4.CollectionProduct],
         bundles: Iterable[pds4.BundleProduct],
         old_base: str,
-        new_base: str):
+        new_base: str,
+        dry: bool):
     """
     Copies products that have already been superseded to a new directory. Since these have already been superseded,
     no manipulations to their path should be necessary.
     """
     for bundle in bundles:
-        copy_to_path(bundle.label_path, paths.relocate_path(bundle.label_path, old_base, new_base))
+        copy_to_path(bundle.label_path, paths.relocate_path(bundle.label_path, old_base, new_base), dry)
     for collection in collections:
-        copy_to_path(collection.label_path, paths.relocate_path(collection.label_path, old_base, new_base))
-        copy_to_path(collection.inventory_path, paths.relocate_path(collection.inventory_path, old_base, new_base))
+        copy_to_path(collection.label_path, paths.relocate_path(collection.label_path, old_base, new_base), dry)
+        copy_to_path(collection.inventory_path, paths.relocate_path(collection.inventory_path, old_base, new_base), dry)
     for product in products:
-        copy_to_path(product.label_path, paths.relocate_path(product.label_path, old_base, new_base))
+        copy_to_path(product.label_path, paths.relocate_path(product.label_path, old_base, new_base), dry)
         for data_path in product.data_paths:
             if os.path.exists(data_path):
-                copy_to_path(data_path, paths.relocate_path(data_path, old_base, new_base))
+                copy_to_path(data_path, paths.relocate_path(data_path, old_base, new_base), dry)
 
 
-def copy_unmodified_collections(collections: Iterable[pds4.Pds4Product], old_base: str, new_base: str) -> None:
+def copy_unmodified_collections(collections: Iterable[pds4.Pds4Product], old_base: str, new_base: str, dry: bool) -> None:
     """
     Copies collection labels and inventories that should be passed through as-is to a new directory
     """
     for c in collections:
         if isinstance(c, pds4.CollectionProduct):
             new_path = paths.relocate_path(paths.generate_product_path(c.inventory_path), old_base, new_base)
-            copy_to_path(c.inventory_path, new_path)
+            copy_to_path(c.inventory_path, new_path, dry)
         else:
             logger.info(f'Skipping non-collection product: {c.label.identification_area.lidvid}')
 
 
-def do_copy_inventory(collections: Iterable[pds4.Pds4Product], old_base, new_base, superseded=False) -> None:
+def do_copy_inventory(collections: Iterable[pds4.Pds4Product], old_base, new_base, superseded=False, dry=False) -> None:
     """
     Copies the collection inventories of a collection product to a new directory
     """
@@ -226,12 +240,12 @@ def do_copy_inventory(collections: Iterable[pds4.Pds4Product], old_base, new_bas
             vid = c.label.identification_area.lidvid.vid
             versioned_path = paths.generate_product_path(d, superseded=superseded, vid=vid)
             new_path = paths.relocate_path(versioned_path, old_base, new_base)
-            copy_to_path(d, new_path)
+            copy_to_path(d, new_path, dry)
         else:
             logger.info(f'Skipping non-collection product: {c.label.identification_area.lidvid}')
 
 
-def do_copy_data(products: Iterable[pds4.Pds4Product], old_base, new_base, superseded=False) -> None:
+def do_copy_data(products: Iterable[pds4.Pds4Product], old_base, new_base, dry: bool, superseded=False) -> None:
     """
     Copies the data files of a basic product to another directory
     """
@@ -241,12 +255,12 @@ def do_copy_data(products: Iterable[pds4.Pds4Product], old_base, new_base, super
                 vid = p.label.identification_area.lidvid.vid
                 versioned_path = paths.generate_product_path(d, superseded=superseded, vid=vid)
                 new_path = paths.relocate_path(versioned_path, old_base, new_base)
-                copy_to_path(d, new_path)
+                copy_to_path(d, new_path, dry)
         else:
             logger.info(f'Skipping non-basic product: {p.label.identification_area.lidvid}')
 
 
-def do_copy_readme(products: Iterable[pds4.BundleProduct], old_base, new_base, superseded=False) -> None:
+def do_copy_readme(products: Iterable[pds4.BundleProduct], old_base, new_base, superseded=False, dry = False) -> None:
     """
     Copies the readme file of a bundle product to another directory
     """
@@ -255,18 +269,19 @@ def do_copy_readme(products: Iterable[pds4.BundleProduct], old_base, new_base, s
             vid = p.label.identification_area.lidvid.vid
             versioned_path = paths.generate_product_path(p.readme_path, superseded=superseded, vid=vid)
             new_path = paths.relocate_path(versioned_path, old_base, new_base)
-            copy_to_path(p.readme_path, new_path)
+            copy_to_path(p.readme_path, new_path, dry)
 
 
-def copy_to_path(src_path: str, dest_path: str):
+def copy_to_path(src_path: str, dest_path: str, dry: bool):
     """
     Copies files from one path to another. Essentially a wrapper for shutil.copy that logs the copy operation
     and makes sure that all of the parent directories exist.
     """
     logger.debug(f'{src_path} -> {dest_path}')
-    dirname = os.path.dirname(dest_path)
-    os.makedirs(dirname, exist_ok=True)
-    shutil.copy(src_path, dest_path)
+    if not dry:
+        dirname = os.path.dirname(dest_path)
+        os.makedirs(dirname, exist_ok=True)
+        shutil.copy(src_path, dest_path)
 
 
 def find_products_to_supersede(previous_products: List[pds4.Pds4Product],
