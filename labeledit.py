0efd0631521295433da27433d12829c447b228ec
Jesse Stone <jstone@psi.edu>
2025-01-22 11:19:29 -0700

    extracted _text_element

--- labeledit.py
+++ labeledit.py
@@ -19,16 +19,14 @@ def inject_bundle_member_entries(labelpath: str, entries_to_add: Iterable[Bundle
 def _bundle_member_entry_to_element(entry: BundleMemberEntry):
     bundle_member_entry = etree.Element("Bundle_Member_Entry")
 
-    lidvid_reference = etree.Element("lidvid_reference")
-    lidvid_reference.text = entry.livdid_reference
-    bundle_member_entry.append(lidvid_reference)
-
-    member_status = etree.Element("member_status")
-    member_status.text = entry.member_status
-    bundle_member_entry.append(member_status)
-
-    reference_type = etree.Element("reference_type")
-    reference_type.text = entry.reference_type
-    bundle_member_entry.append(reference_type)
+    bundle_member_entry.append(_text_element("lidvid_reference", entry.livdid_reference))
+    bundle_member_entry.append(_text_element("member_status", entry.member_status))
+    bundle_member_entry.append(_text_element("reference_type", entry.reference_type))
 
     return bundle_member_entry
+
+
+def _text_element(name: str, value: str):
+    elem = etree.Element(name)
+    elem.text = value
+    return elem
